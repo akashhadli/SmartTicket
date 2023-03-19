15211bf7e714847bd1d3449973c68759f4d36703
Aakash Hadli <82946481+akashhadli@users.noreply.github.com>
2023-03-19 10:56:01 +0530

    set password for both user and employee

--- server/controllers/SetPassword/setPassword.js
+++ server/controllers/SetPassword/setPassword.js
@@ -2,40 +2,66 @@ const db = require('../../db/db');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
-//SET OR RESET USER PASSWORD
+//SET USER PASSWORD
 exports.setPassword = (req, res) => {
-  let tblCommuter = req.body;
-  let id = tblCommuter.UserId;
-  bcrypt.hash(tblCommuter.UPassword, saltRounds, (err, hash) => {
-    if (!err) {
-      query = 'UPDATE tblCommuter SET UPassword = ? WHERE UserId = ?';
-      db.query(query, [hash, id], (err, results) => {
-        if (!err) {
-          if (results.affectedRows == 0) {
-            return res.status(404).json({ message: 'User ID not Found' });
-          }
-          updtPassInAuth(hash, id);
-          return res.status(200).json({ message: 'Password Set' });
-        } else {
-          return res.status(500).json(err);
-        }
-      });
-    } else {
-      console.log(err);
-    }
-  });
+	let setdata = req.body;
+	console.log(setdata);
+	if (setdata.flag == 'U') {
+		let uid = setdata.Id;
+		bcrypt.hash(setdata.Password, saltRounds, (err, hash) => {
+			if (!err) {
+				query = 'UPDATE tblCommuter SET UPassword = ? WHERE UserId = ?';
+				db.query(query, [hash, uid], (err, results) => {
+					if (!err) {
+						if (results.affectedRows == 0) {
+							return res.status(404).json({ message: 'User ID not Found' });
+						}
+						updtPassInAuth(hash, uid);
+						return res.status(200).json({ message: 'User Password Set' });
+					} else {
+						return res.status(500).json(err);
+					}
+				});
+			} else {
+				console.log(err);
+			}
+		});
+	}
+	// SET EMPLOYEE PASSWORD
+	else if (setdata.flag == 'E') {
+		let eid = setdata.Id;
+		bcrypt.hash(setdata.Password, saltRounds, (err, hash) => {
+			if (!err) {
+				query = 'UPDATE tblEmployee SET EmpPassword = ? WHERE EmpId = ?';
+				db.query(query, [hash, eid], (err, results) => {
+					if (!err) {
+						if (results.affectedRows == 0) {
+							return res.status(404).json({ message: 'Employee ID not Found' });
+						}
+						updtPassInAuth(hash, eid);
+						return res.status(200).json({ message: 'Employee Password Set' });
+					} else {
+						return res.status(500).json(err);
+					}
+				});
+			} else {
+				console.log(err);
+			}
+		});
+	}
+	//   return res.status(500).json(err);
 };
 
 //UPDATE PASSWORD IN AUTHTABLE
 const updtPassInAuth = (password, id) => {
-  var query = 'UPDATE tblAuth SET Password = ? WHERE AuthId = ?';
-  db.query(query, [password, id], (err, results) => {
-    if (!err) {
-      if (results.affectedRows == 0) {
-        console.log('User not found');
-      } else {
-        console.log('Password set in auth');
-      }
-    }
-  });
+	var query = 'UPDATE tblAuth SET Password = ? WHERE AuthId = ?';
+	db.query(query, [password, id], (err, results) => {
+		if (!err) {
+			if (results.affectedRows == 0) {
+				console.log('User not found');
+			} else {
+				console.log('Password set in auth');
+			}
+		}
+	});
 };
