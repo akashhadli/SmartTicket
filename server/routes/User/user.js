2aad79320467a5e1581ba4ff37769b9f21e282ab
Akash Hadli <aakashhadli@gmail.com>
2023-03-28 10:50:55 +0530

    EmpType Column in tblAuth

--- server/routes/User/user.js
+++ server/routes/User/user.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-	register,
-	profile,
-	editprofile,
-} = require('../../controllers/User/user');
+const { register } = require('../../controllers/User/user');
 
 router.route('/register').post(register);
-router.route('/profile').get(profile);
-router.route('/profile/edit').patch(editprofile);
 
 module.exports = router;
