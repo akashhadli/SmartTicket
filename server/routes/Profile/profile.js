bde7592834b336e16badeb61fa8f769e7c0555e0
Aakash Hadli <aakashhadli@gmail.com>
2023-03-31 12:24:55 +0530

    send flag data in response

--- server/routes/Profile/profile.js
+++ server/routes/Profile/profile.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { Profile } = require('../../controllers/Profile/profile');
+const { Profile, Edit } = require('../../controllers/Profile/profile');
 
 router.route('/').post(Profile);
+router.route('/edit').post(Edit);
 
 module.exports = router;
